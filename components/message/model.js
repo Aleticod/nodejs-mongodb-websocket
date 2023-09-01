c4ba40aa1b7d9488fd8228601295037e4b847625
Aleticod <193837@unsaac.edu.pe>
2023-08-31 23:21:47 -0500

    :sparkles: Add MongoDB conection and save data

---
+++ components/message/model.js
@@ -0,0 +1,20 @@
+// Importamos mongoose
+const mongoose = require('mongoose');
+
+// Creamos una instancia de la clase Schema
+const Schema = mongoose.Schema;
+
+// Creamos un Schema para nuestra base de datos
+const mySchema = new Schema({
+    user: String,
+    message: {
+        type: String,
+        required: true
+    },
+    date: Date
+});
+
+// Creamos un modelo con el schema anterior
+const model = mongoose.model('Message', mySchema);
+
+module.exports = model;
--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,25 @@
-const list = [];
+// Importamos nuestro modelo
+const db = require('mongoose');
+const Model = require('./model');
+
+// Pedir a mongoose que use promesas
+db.Promise = global.Promise;
+
+// Haremos una conexion de la base de datos
+db.connect('mongodb://root:root@127.0.0.1:27017/?authMechanism=DEFAULT&tls=false', {useNewUrlParser: true, useUnifiedTopology: true, dbName: 'telegram'});
+
+console.log('[db]: Conectada con exito');
 
 function addMessage(message) {
-    list.push(message);
+    //list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save()
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    // return list;
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
