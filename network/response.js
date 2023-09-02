88b5272d0d4880d217129ab2c527bb3f5828231c
Aleticod <193837@unsaac.edu.pe>
2023-09-01 19:23:30 -0500

    :sparkles: Add status code messages into network/response

--- network/response.js
+++ network/response.js
@@ -1,15 +1,40 @@
+const statusMessages = {
+    '200': 'Done',
+    '201': 'Created',
+    '400': 'Invalid format',
+    '500': 'Internal error'
+}
+
 exports.success = function(req, res, message, status) {
     // res.send(message);
-    res.status(status || 200).send({
+    let statusCode = status;
+    let statusMessage = message;
+    if (!status) {
+        statusCode = 200;
+    }
+
+    if (!message) {
+        statusMessage = statusMessages[statusCode];
+    }
+    res.status(statusCode).send({
         error: "",
-        body: message
+        body: statusMessage
     })
 }
 
 exports.error = function(req, res, message, status, details) {
     console.error('[response error] ' + details); // Esto es un log
-    res.status(status || 500).send({
-        error: message,
+    let statusCode = status;
+    let statusMessage = message;
+    if (!status) {
+        statusCode = 500;
+    }
+
+    if (!message) {
+        statusMessage = statusMessages[statusCode];
+    }
+    res.status(statusCode).send({
+        error: statusMessage,
         body: ""
     })
 }
